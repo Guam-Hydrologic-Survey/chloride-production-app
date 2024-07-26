f3821d7998fceb7e271d668cb92bff3106197c92
Dannika <71364766+dannikate@users.noreply.github.com>
2024-07-26 12:05:46 +1000

    adjust modal title; add id

--- static/components/About.js
+++ static/components/About.js
@@ -12,7 +12,7 @@ export function About(element) {
       <div class="modal-dialog modal-dialog-centered modal-xl">
         <div class="modal-content">
           <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">About</h1>
+            <h1 class="modal-title fs-4" id="about-modal-title">About</h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
