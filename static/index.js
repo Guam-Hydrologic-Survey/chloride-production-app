05b3fae0fa3be56d31cffcb9546e848868e2d827
Dannika <71364766+dannikate@users.noreply.github.com>
2024-07-30 11:49:19 +1000

    use summary v2
    update import

--- static/index.js
+++ static/index.js
@@ -7,7 +7,7 @@ import { NavBar } from "./components/NavBar.js";
 import { About } from "./components/About.js";
 import { LMap } from "./components/LMap.js";
 // import { Legend } from "./components/Legend.js";
-import { Summary } from "./components/Summary.js";
+import { Summary } from "./components/Summary_v2.js";
 import { PointInfo } from "./components/PointInfo.js";
 
 // tests
