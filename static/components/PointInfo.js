95fd5110e4521bcd94ecf3145112fe24712427d5
Dannika <71364766+dannikate@users.noreply.github.com>
2024-07-31 12:40:37 +1000

    update id for reset btn, add event listener on modal close

--- static/components/PointInfo.js
+++ static/components/PointInfo.js
@@ -4,14 +4,17 @@ PointInfo.js
 
 const modalId = "point-info-modal";
 const modalHeaderTitleId = "point-info-header";
+
 const statsContentId = "stats-content";
 const plotContentId = "plot-content";
+
+const resetBtnId = "plot-zoom-latest-data";
 
 export function PointInfo(element) {
 
     element.innerHTML = /*html*/ 
     `
-    <div class="modal fade" id="point-info-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="${modalId}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-xl modal-dialog-centered">
             <div class="modal-content">
             <div class="modal-header">
@@ -25,14 +28,18 @@ export function PointInfo(element) {
                 <div id="${statsContentId}"></div> <!-- previously #modal-body-content -->
                 <div class="vl"></div>
                 <div>
-                    <button type="button" class="btn btn-primary" id="plot-zoom-latest-data">Latest Data</button>
                     <div id="${plotContentId}"></div> <!-- previously #large-plot -->
+                    <button type="button" class="btn btn-primary" id="${resetBtnId}" title="View latest data values for chloride and production">Latest Data</button>
                 </div>
             </div>
             </div>
         </div>
     </div>
     `;
+
+    document.getElementById(modalId).addEventListener('hidden.bs.modal', () => {
+        document.getElementById(resetBtnId).textContent = "Latest Data";
+    });
 }
 
 export { statsContentId, plotContentId, modalId, modalHeaderTitleId }
