a172c8f4ed27cb9a3096894082d79392474eb158
sydneymaq <144676993+sydneymaq@users.noreply.github.com>
2026-09-11 14:38:31 +1000

    initial commit, only added new chloride files an updated main.js still need to add summary history

--- static/main.js
+++ static/main.js
@@ -452,32 +452,32 @@ map.addControl(searchControl);
 const basins = [
 	{
 		"name": "Machanao",
-		"data":	"./static/data/MachanaoBasin10162024RSQUARED.json",
+		"data":	"./static/data/MachanaoBasin092026.json",
 		"color": "#7A8EF5",
 	},
 	{
 		"name": "Upi",
-		"data":	"./static/data/UpiBasin10162024RSQUARED.json",
+		"data":	"./static/data/UpiBasin092026.json",
 		"color": "blue",
 	},
 	{
 		"name": "Mangilao",
-		"data":	"./static/data/MangilaoBasin10162024RSQUARED.json",
+		"data":	"./static/data/MangilaoBasin092026.json",
 		"color": "red",
 	},
 	{
 		"name": "Finegayan",
-		"data": "./static/data/FinegayanBasin10162024RSQUARED.json",
+		"data": "./static/data/Finagua'yokBasin092026.json",
 		"color": "green",
 	},
 	{
 		"name": "Hagåtña",
-		"data": "./static/data/HagåtñaBasin10162024RSQUARED.json",
+		"data": "./static/data/HagåtñaBasin092026.json",
         "color": "#FFAA00",
 	},
 	{
 		"name": "Yigo-Tumon",
-		"data":	"./static/data/Yigo-TumonBasin10162024RSQUARED.json",
+		"data":	"./static/data/TomhomBasin092026.json",
         "color": "#73DFFF",
 	},
 ];
